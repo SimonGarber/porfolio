dad9e2f1d9eaefcf693c639eb9f8fe59d9a21664
Simon Garber <Simon.garber@gmail.com>
2019-10-28 14:49:12 -0400

    tested the cli

--- porfolio/src/App.vue
+++ porfolio/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="New Application" />
+    <HelloWorld msg="Vue Portfolio" />
   </div>
 </template>
 
